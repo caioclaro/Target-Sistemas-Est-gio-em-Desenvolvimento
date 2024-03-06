c23ea5822954054f078e96e7c59296572984247f
caioclaro <159395680+caioclaro@users.noreply.github.com>
2024-03-05 22:36:03 -0300

    Update and rename README.md to README.py

---
+++ README.py
@@ -0,0 +1,16 @@
+mensagem = """
+Prezados,
+
+Gostaria de expressar minha sincera gratidão pela oportunidade de tentar a vaga de estágio na empresa Target Sistemas. 
+
+Foi um prazer participar do processo seletivo e conhecer mais sobre a cultura e os desafios que a empresa enfrenta. 
+
+Estou muito animado com a possibilidade de contribuir para a equipe e aprender com os profissionais experientes da Target Sistemas.
+
+Agradeço novamente pela consideração e espero ter a oportunidade de trabalhar com vocês no futuro.
+
+Atenciosamente,
+[Caio Claro]
+"""
+
+print(mensagem)
